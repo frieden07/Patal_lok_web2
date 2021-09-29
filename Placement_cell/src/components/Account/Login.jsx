ddd0f1d6ca0924df5662f3cd744b3de2ef971690
Shivanshu Tiwari <tiwarishivanshu535@gmail.com>
2021-09-29 10:51:16 +0530

    Add files via upload

---
+++ Placement_cell/src/components/Account/Login.jsx
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react'
+import "./Login.css"
+import { Link } from 'react-router-dom'
+import avatar from "../images_source/login.gif"
+const Login = () => {
+    return (
+        <Fragment>
+            <form>
+
+                <div class="logincontainer">
+                    <h2 style={{ textAlign: "center" }}>Log In</h2>
+                    <div class="imgcontainer">
+                        <img src={avatar} alt="Avatar" class="avatar" />
+                    </div>
+                    <br />
+                    <label for="uname"><b>Username</b></label>
+                    <input type="text" placeholder="Enter Username" name="username" required />
+                    <br /><br />
+                    <label for="password"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="password" required />
+
+                    <br /><br />
+                    <button style={{ width: "90px", borderRadius: "25px" }} type="submit">Log In</button>
+
+                    <Link className="newacc" to="/signup">Create New Account</Link>
+                </div>
+
+            </form>
+        </Fragment>
+    )
+}
+
+export default Login
