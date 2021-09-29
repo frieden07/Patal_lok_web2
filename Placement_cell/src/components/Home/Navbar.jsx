edddea4904e8414facc9daf66de31e0c638fb333
Shivanshu Tiwari <tiwarishivanshu535@gmail.com>
2021-09-29 19:58:23 +0530

    Update Navbar.jsx

--- Placement_cell/src/components/Home/Navbar.jsx
+++ Placement_cell/src/components/Home/Navbar.jsx
@@ -8,7 +8,6 @@ const Navbar = () => {
         <Fragment>
             <nav>
                 <Link className="navbar" to="/">Home</Link>
-                <Link className="navbar" to="/stats">Stats</Link>
                 <Link className="navbar" to="/Admin">Admin</Link>
                 <Link className="navbar" to="/user">User</Link>
                 <Link className="navbar_login" to="/login">Log In/Sign Up</Link>
